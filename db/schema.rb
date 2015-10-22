bcac609ee77cbd637edc26309141dcf804cfccc1
KemalC <ddeveloperr@gmail.com>
2015-10-22 10:56:41 +0000

    Make app with basic functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827203355) do
+ActiveRecord::Schema.define(version: 20151022102652) do
+
+  create_table "categories", force: true do |t|
+  end
 
   create_table "listings", force: true do |t|
     t.string   "title"
